da97d192883fb8f28c3bc1158b09610cfcf1db7b
parlyresk <parlyresk325@gmail.com>
2024-07-25 10:32:49 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -5,98 +5,24 @@ import Emaillist from './Emaillist';
 import './assets/scss/App.scss';
 
 function App() {
-    const [emails, setEmails] = useState(null);
-
-    const deleteEmail = async (no) => {
-        try {
-            const response = await fetch(`/api/delete/${no}`, {
-                method: 'DELETE',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error(`${response.status} ${response.statusText}`);
-            }
-
-            const json = await response.json();
-
-            if (json.result !== 'success') {
-                throw new Error(json.message);
-            }
-
-            setEmails(emails.filter(email => email.no !== no));
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    const addEmail = async (email) => {
-        try {
-            const response = await fetch('/api', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(email)
-            });
-
-            if (!response.ok) {
-                throw new Error(`${response.status} ${response.statusText}`);
-            }
-
-            const json = await response.json();
-
-            if (json.result !== 'success') {
-                throw new Error(json.message);
-            }
-
-            setEmails([json.data, ...emails]);
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    const fetchEmails = async (keyword) => {
-        try {
-            const response = await fetch(`/api?kw=${keyword ? keyword : ''}`, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error(`${response.status} ${response.statusText}`);
-            }
-
-            const json = await response.json();
-
-            if (json.result !== 'success') {
-                throw new Error(json.message);
-            }
-
-            setEmails(json.data);
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    useEffect(() => {
-        fetchEmails();
-    }, []);
-
-    return (
-        <div id={'App'}>
-            <RegisterForm addEmail={addEmail} />
-            <SearchBar fetchEmails={fetchEmails} />
-            <Emaillist emails={emails} deleteEmail={deleteEmail} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Reactasdasd
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
